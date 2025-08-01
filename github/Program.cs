c05587152d0e88465d5ddf95d3d656255852dc9c
Nycolas Mariano <nycolas.mariano@portalsesisp.org.br>
2025-08-01 08:29:54 -0300

    Adicionar arquivos de projeto.

---
+++ github/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace github
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello word");
+
+            Console.WriteLine("This is a test of the GitHub repository.");
+        }
+    }
+}
